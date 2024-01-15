5fcccd443ae95a1f8d16baabb0ab2d5c7974e2e5
Nikhil <np0129999@gmail.com>
2024-01-15 18:54:28 +0530

    level 2 - Arrays and string lecture 2 codes added

--- pepcoding/Level 2/5.Searching and sorting/l002_lecture2.java
+++ pepcoding/Level 2/5.Searching and sorting/l002_lecture2.java
@@ -42,4 +42,62 @@ public class l002_lecture2 {
 
         return false;
     }
+
+    // (IMP)3. count inversions 
+    // https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+    // time complexity -> nlogn+n i.e  O(nlogn), space => O(n) 
+    /**
+     * 1. we can't use quick sort as it will change elements order at start only
+     * 2. we will use merge sort as we will perform sorting in post order so it will not effect our inversions
+     * we will calculate inversions first then sort
+     */
+    static long inversionCount(long arr[], long N)
+    {
+        // Your Code Here 
+        int n = (int)N;
+        return mergeSort(arr,0,n-1);
+    }
+    
+    static long mergeSort(long[] arr,int si, int ei){
+        if(si>=ei)
+            return 0;
+            
+        int mid = si+(ei-si)/2;
+        long leftInversions = mergeSort(arr,si,mid);
+        long rightInversions = mergeSort(arr,mid+1,ei);
+        
+        long acrossInversions = mergeTwoSortedArrays(arr,si,mid,mid+1,ei);
+        
+        return (leftInversions+rightInversions+acrossInversions);
+    }
+    
+    static long mergeTwoSortedArrays(long[] arr, int si1,int ei1,int si2,int ei2){
+        
+        long[] temp = new long[ei1-si1+ei2-si2+2];
+        
+        int ptr1 = si1 , ptr2 = si2, idx = 0 ;
+        long count = 0;
+        
+        
+        while(ptr1<=ei1 && ptr2<=ei2){
+            
+            if(arr[ptr1]>arr[ptr2]){
+                count += (ei1-ptr1+1);
+                temp[idx++] = arr[ptr2++];
+            }else{
+                temp[idx++] = arr[ptr1++];
+            }
+        }
+        
+        while(ptr1<=ei1)
+            temp[idx++] = arr[ptr1++];
+            
+        while(ptr2<=ei2)
+            temp[idx++] = arr[ptr2++];
+            
+        for(int i=0 ; i<temp.length ; i++)
+            arr[si1+i] = temp[i];
+            
+        return count ;
+    }
 }
